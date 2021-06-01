6038a018574cdcfc68439eb1953d65d48e8f324b
ikrakel <franciscomleal@gmail.com>
2021-06-01 22:36:49 +0100

    Add standard rb for code consistency

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  namespace :api, constraints: { format: :json } do
+  namespace :api, constraints: {format: :json} do
     namespace :v1 do
       resources :users
     end
   end
 
-  get "investor(/*path)" => "pages#investor", as: :investor
-  get "talent(/*path)" => "pages#talent", as: :talent
-  get "admin(/*path)" => "pages#admin", as: :admin
-  
+  get "investor(/*path)" => "pages#investor", :as => :investor
+  get "talent(/*path)" => "pages#talent", :as => :talent
+  get "admin(/*path)" => "pages#admin", :as => :admin
+
   root to: "pages#home", as: :root
 end
