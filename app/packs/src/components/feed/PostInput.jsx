cbfc2bc579b56d1e8a983b3731c6eb11289da902
francisco-leal <francisco@talentprotocol.com>
2021-07-21 17:35:29 +0100

    Add Feed input

---
+++ app/packs/src/components/feed/PostInput.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { post } from "src/utils/requests"
+
+import TalentProfilePicture from '../talent/TalentProfilePicture'
+
+const PostInput = ({ profilePictureUrl }) => {
+  const [text, setText] = useState("")
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    post(
+      `/posts`,
+      { text }
+    ).then((response) => {
+      if(response.error) {
+        console.log(response.error)
+      } else {
+        setText("")
+      }
+    })
+  }
+
+  return (
+    <form action={`/posts`} method="post" onSubmit={onSubmit} className="d-flex flex-row p-2 w-100">
+      <TalentProfilePicture src={profilePictureUrl} height={40}/>
+      <textarea id="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Start a post.." className="mx-3 form-control"/>
+      <button type="submit" className="btn btn-primary btn-small">
+        Create
+      </button>
+    </form>
+  )
+}
+
+export default PostInput
