f662dbc659fc39fdec891b020e6dea3666e59a62
francisco-leal <francisco@talentprotocol.com>
2021-07-30 10:01:34 +0100

    Truncate text on talent card and add white-space to descriptions

--- app/packs/src/components/talent/TalentShow.jsx
+++ app/packs/src/components/talent/TalentShow.jsx
@@ -12,7 +12,7 @@ const CareerGoals = ({ careerGoals }) => {
   return (
     <div className="mb-3 mb-md-5">
       <h6 className="talent-show-h6 p-2 d-inline">CAREER GOALS</h6>
-      {careerGoals.map((careerGoal, index) => (<p className="mt-3 mb-0" key={`career-goal-${index}`}>{careerGoal.description}</p>))}
+      {careerGoals.map((careerGoal, index) => (<p className="mt-3 mb-0 talent-long-description" key={`career-goal-${index}`}>{careerGoal.description}</p>))}
     </div>
   )
 }
@@ -37,7 +37,7 @@ const AboutMe = ({ description, youtubeUrl }) => {
   return (
     <div className="mb-3 mb-md-5">
       <h6 className="talent-show-h6 p-2 d-inline mb-3">ABOUT ME</h6>
-      <p className="mt-3 mb-0" >{description}</p>
+      <p className="mt-3 mb-0 talent-long-description" >{description}</p>
       {youtubeUrl && <div className="talent-profile-video-player mt-3 mx-auto">
         <ReactPlayer url={youtubeUrl} light width={"100%"} height={"100%"}/>
       </div>}
@@ -46,7 +46,6 @@ const AboutMe = ({ description, youtubeUrl }) => {
 }
 
 const TalentDetail = ({ profilePictureUrl, username, ticker, tags, linkedinUrl }) => {
-
   return (
     <div className="mb-3 mb-md-5 d-flex flex-column flex-md-row align-items-center">
       <TalentProfilePicture src={profilePictureUrl} height={96}/>
--- app/packs/src/components/talent/TalentTags.jsx
+++ app/packs/src/components/talent/TalentTags.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TalentTags = ({ tags, talent_id }) => {
   if (tags && tags.length > 0) {
     return (
-      <div className="d-flex flex-row">
+      <div className="d-flex flex-row flex-wrap">
         {tags.map((tag) =>
           (<div key={`${talent_id}_${tag}`} className="text-regular px-1 mr-2 border border-light rounded-pill">
             <small><strong>{tag}</strong></small>
