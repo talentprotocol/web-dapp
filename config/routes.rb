cbe2d012224754e1b7acf649aa905ce1927a440d
francisco-leal <francisco@talentprotocol.com>
2021-08-04 14:49:14 +0100

    remove sign up views

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,6 @@ Rails.application.routes.draw do
 
   get "/sign_in" => "sessions#new", :as => "sign_in"
   delete "/sign_out" => "sessions#destroy", :as => "sign_out"
-  # get "/sign_up" => "clearance/users#new", :as => "sign_up"
 
   # end Auth
 
