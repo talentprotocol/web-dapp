76ca5c195925115ead84a3622e1e4fa6bf7cf4d6
francisco-leal <francisco@talentprotocol.com>
2021-08-12 00:49:55 +0100

    Add rollback to create transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -31,7 +31,7 @@ class Transaction < ApplicationRecord
       priceInTal: coin.display_price_in_tal,
       value: display_value,
       valueInTal: display_value_in_tal,
-      priceVariance7d: prng.rand(1...13.5).truncate(2).to_s,
+      priceVariance7d: "0",
       profilePictureUrl: coin.talent.profile_picture_url,
       inbound: inbound
     }
