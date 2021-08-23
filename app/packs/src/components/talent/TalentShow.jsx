bc27b9171bb12d5111f4fa1166bd0bb2127bccab
francisco-leal <francisco@talentprotocol.com>
2021-08-23 08:45:01 +0100

    Improve public/private mode in talent profile

--- app/packs/src/components/talent/TalentShow.jsx
+++ app/packs/src/components/talent/TalentShow.jsx
@@ -14,37 +14,9 @@ const TalentShow = ({
   tokenDeployed,
 }) => {
   const talentIsFromCurrentUser = talent.userId == currentUserId;
-  const [publicProfile, setPublicProfile] = useState(talent.public);
-  const [error, setError] = useState(false);
-  const [message, setMessage] = useState("");
-
-  const togglePublic = async () => {
-    const response = await patch(`/talent/${talent.id}`, {
-      talent: { public: !publicProfile },
-    });
-    if (response.error) {
-      setError(true);
-      setMessage("We had an issue updating your profile");
-    } else {
-      setError(false);
-      setMessage(
-        `Your profile is now ${!publicProfile ? "public" : "private"}`
-      );
-      setPublicProfile(!publicProfile);
-    }
-  };
 
   return (
     <div className="d-flex flex-column">
-      {message !== "" && (
-        <div
-          className={`mt-2 alert alert-${
-            error ? "danger" : "success"
-          } talent-alert talent-alert-${error ? "danger" : "success"}`}
-        >
-          {message}
-        </div>
-      )}
       <TalentDetail
         username={talent.username}
         profilePictureUrl={talent.profilePictureUrl}
@@ -54,6 +26,7 @@ const TalentShow = ({
         allowEdit={talentIsFromCurrentUser}
         talentId={talent.id}
         tokenDeployed={tokenDeployed}
+        publicProfile={talent.publicProfile}
       />
       <AboutMe
         description={talent.description}
@@ -72,15 +45,6 @@ const TalentShow = ({
         allowEdit={talentIsFromCurrentUser}
         talentId={talent.id}
       />
-      {talentIsFromCurrentUser && (
-        <button
-          role="button"
-          onClick={togglePublic}
-          className="talent-button btn btn-warning"
-        >
-          Make {!publicProfile ? "public" : "private"}
-        </button>
-      )}
     </div>
   );
 };
