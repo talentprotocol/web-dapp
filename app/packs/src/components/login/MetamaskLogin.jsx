6767face5a2e4ea2e7b6b7b9004c75974ef47f10
francisco-leal <francisco@talentprotocol.com>
2021-09-02 09:45:50 +0100

    Improve login flow with error messages and disabling the button

--- app/packs/src/components/login/MetamaskLogin.jsx
+++ app/packs/src/components/login/MetamaskLogin.jsx
@@ -6,6 +6,7 @@ import { post, get } from "../../utils/requests";
 const MetamaskLogin = ({}) => {
   const [provider, setProvider] = useState(null);
   const [requestingMetamask, setRequestingMetamask] = useState("false");
+  const [error, setError] = useState("");
 
   useEffect(() => {
     detectEthereumProvider({ mustBeMetaMask: true }).then(
@@ -19,6 +20,7 @@ const MetamaskLogin = ({}) => {
 
   const loginWithMetamask = (e) => {
     e.preventDefault();
+    let timeoutRef;
 
     setRequestingMetamask("true");
     if (provider) {
@@ -26,8 +28,10 @@ const MetamaskLogin = ({}) => {
         if (accounts.length > 0) {
           return get(`/users?wallet_id=${accounts[0]}`).then((result) => {
             if (result.error) {
+              setError("404Wallet");
               setRequestingMetamask("false");
             } else {
+              timeoutRef = setTimeout(() => setError("PendingMetamask"), 5000);
               return provider
                 .request({
                   method: "personal_sign",
@@ -37,15 +41,22 @@ const MetamaskLogin = ({}) => {
                   ],
                 })
                 .then((result) => {
+                  clearTimeout(timeoutRef);
+
                   return post("/session", {
                     signed_message: result,
                     wallet_id: accounts[0],
                   }).then((response) => {
                     if (response.success) {
                       window.location.href = response.success;
+                    } else {
+                      setError("BadAuth");
+                      setRequestingMetamask("false");
                     }
-                    setRequestingMetamask("false");
                   });
+                })
+                .catch(() => {
+                  setRequestingMetamask("false");
                 });
             }
           });
@@ -54,16 +65,30 @@ const MetamaskLogin = ({}) => {
     }
   };
 
-  const allowConnect = () => {
-    requestingMetamask == "false" && provider != null;
-  };
+  const allowConnect = () => requestingMetamask == "false" && provider != null;
 
   const buttonText = requestingMetamask == "false" ? "Log in" : "Logging in...";
 
   return (
     <form onSubmit={loginWithMetamask} className="d-flex flex-column">
+      {error == "404Wallet" && (
+        <p className="text-danger">We don't recognize this wallet address.</p>
+      )}
+      {error == "BadAuth" && (
+        <>
+          <p className="text-danger">
+            We were unable to verify your signature.
+          </p>
+          <p className="text-danger">If the problem persists contact us.</p>
+        </>
+      )}
+      {error == "PendingMetamask" && (
+        <p className="text-warning">
+          Check your metamask for a pending signature request.
+        </p>
+      )}
       <button
-        disable={allowConnect()}
+        disabled={!allowConnect()}
         type="submit"
         className="btn btn-primary talent-button"
       >
