57b07df3ab21bc7736faa6410d9ae37678e2a28c
Frederico Moura <verdefred@hotmail.com>
2021-09-13 19:34:02 +0100

    Sendgrid setup

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,6 @@
 Clearance.configure do |config|
   config.routes = false
-  config.mailer_sender = "reply@example.com"
+  config.mailer_sender = "no-reply@talentprotocol.com"
   config.rotate_csrf_on_sign_in = true
   config.allow_sign_up = false
   config.sign_in_guards = ["UpdateUserMetadataGuard"]
