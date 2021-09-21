0efafb9b93d76630b9a4f4fe0528742deb3b0bea
francisco-leal <francisco@talentprotocol.com>
2021-09-21 14:12:38 +0100

    List details on profile

--- app/packs/src/components/talent/TalentProfilePicture.jsx
+++ app/packs/src/components/talent/TalentProfilePicture.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import DefaultProfilePicture from "images/default-profile-icon.jpg";
 
-const TalentProfilePicture = ({ src, height, greyscale, className }) => {
+const TalentProfilePicture = ({
+  src,
+  height,
+  greyscale,
+  className,
+  straight,
+}) => {
   const imgSrc = src || DefaultProfilePicture;
   const grey = greyscale ? " image-greyscale" : "";
+  const roundPhoto = straight ? "" : "rounded-circle";
   return (
     <img
-      className={`rounded-circle${grey} ${className} image-fit`}
+      className={`${roundPhoto}${grey} ${className || ""} image-fit`}
       src={imgSrc}
       width={height}
       height={height}
