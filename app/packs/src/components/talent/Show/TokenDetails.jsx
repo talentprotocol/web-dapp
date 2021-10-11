2a6309b8987b3e6de5b814e1bf8175b9764d6559
francisco-leal <francisco@talentprotocol.com>
2021-10-11 17:27:10 +0100

    Handle null data from thegraph

--- app/packs/src/components/talent/Show/TokenDetails.jsx
+++ app/packs/src/components/talent/Show/TokenDetails.jsx
@@ -30,7 +30,7 @@ const GET_TALENT_PORTFOLIO = gql`
 
 const TokenDetails = ({ token, ticker, displayName }) => {
   const { loading, error, data } = useQuery(GET_TALENT_PORTFOLIO, {
-    variables: { id: token.contract_id.toLowerCase() },
+    variables: { id: token?.contract_id?.toLowerCase() },
   });
 
   const [tokenData, setTokenData] = useState({
@@ -40,14 +40,14 @@ const TokenDetails = ({ token, ticker, displayName }) => {
   });
 
   useEffect(() => {
-    if (loading) {
+    if (loading || !data || !data.talentToken) {
       return;
     }
 
     setTokenData({
       ...tokenData,
-      totalSupply: ethers.utils.formatUnits(data.talentToken.totalSupply),
-      supporterCount: data.talentToken.supporterCounter,
+      totalSupply: ethers.utils.formatUnits(data.talentToken.totalSupply || 0),
+      supporterCount: data.talentToken.supporterCounter || 0,
     });
   }, [data, loading]);
 
@@ -57,7 +57,7 @@ const TokenDetails = ({ token, ticker, displayName }) => {
         <h6 className="card-title">{ticker} Price Statistics</h6>
         <div className="d-flex flex-column justify-content-between">
           <small className="text-muted">Token Address</small>
-          <small>{token.contract_id}</small>
+          <small>{token.contract_id || "Coming soon"}</small>
         </div>
         <h6 className="card-subtitle mb-2 text-muted mt-4">
           {displayName} Price today
