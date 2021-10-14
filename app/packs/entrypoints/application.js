2bd7e80b25add28b7ffc44e612e9119d2eb075f2
francisco-leal <francisco@talentprotocol.com>
2021-10-14 23:20:45 +0100

    [cleanup] remove no longer used components

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -14,18 +14,13 @@ import ReactOnRails from "react-on-rails";
 import Button from "src/components/button";
 import Navbar from "src/components/navbar";
 import Alert from "src/components/alert";
-import TalentCard from "src/components/talent/TalentCard";
 import Pagination from "src/components/pagination";
 import NavTabs from "src/components/navbar/NavTabs";
 import TalentShow from "src/components/talent/TalentShow";
-import TalentLeaderboard from "src/components/leaderboards/TalentLeaderboard";
-import DisplayTokenVariance from "src/components/token/DisplayTokenVariance";
 import MessageUserList from "src/components/chat/MessageUserList";
 import Chat from "src/components/chat/Chat";
 import RegistrationFlow from "src/components/registration/RegistrationFlow";
 import TalentNameSearch from "src/components/talent/TalentNameSearch";
-import MetamaskLogin from "src/components/login/MetamaskLogin";
-import DeployTokenButton from "src/components/token/DeployTokenButton";
 import Logo from "src/components/logo";
 import MetamaskConnect from "src/components/login/MetamaskConnect";
 import TalentSupportersTable from "src/components/supporters/TalentSupportersTable";
@@ -46,18 +41,13 @@ ReactOnRails.register({
   Button,
   Navbar,
   Alert,
-  TalentCard,
   Pagination,
   NavTabs,
   TalentShow,
-  TalentLeaderboard,
-  DisplayTokenVariance,
   MessageUserList,
   Chat,
   RegistrationFlow,
   TalentNameSearch,
-  MetamaskLogin,
-  DeployTokenButton,
   Logo,
   MetamaskConnect,
   TalentSupportersTable,
