75a18d5cfd8da5e9e3dde927c6419ec38ed7821d
francisco-leal <francisco@talentprotocol.com>
2021-10-19 15:24:24 +0100

    if we would skip creating the notification, update the updated at instead

--- app/services/CreateNotification.rb
+++ app/services/CreateNotification.rb
@@ -3,8 +3,12 @@ class CreateNotification
   end
 
   def call(title:, body:, user_id:, type:, source_id: nil)
-    return if Notification.where(user_id: user_id, type: type, read: false, source_id: source_id).count.positive?
-
-    Notification.create!(title: title, body: body, user_id: user_id, type: type, source_id: source_id)
+    notification = Notification.where(user_id: user_id, type: type, read: false, source_id: source_id).last
+    if notification.present?
+      notification.updated_at = Time.current
+      notification.save!
+    else
+      Notification.create!(title: title, body: body, user_id: user_id, type: type, source_id: source_id)
+    end
   end
 end
