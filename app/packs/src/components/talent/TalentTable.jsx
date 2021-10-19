cf2508d8939406f66182eb92bcc9ce7b36edc573
francisco-leal <francisco@talentprotocol.com>
2021-10-19 12:21:41 +0100

    remove console log

--- app/packs/src/components/talent/TalentTable.jsx
+++ app/packs/src/components/talent/TalentTable.jsx
@@ -101,8 +101,6 @@ const TalentTable = ({ talents }) => {
     return 0;
   };
 
-  console.log(follows);
-
   return (
     <div className="table-responsive">
       <table className="table table-hover mb-0 mt-4">
