974045aa90875f7b05be68b46e5bb0ab013f4530
francisco-leal <francisco@talentprotocol.com>
2021-10-19 15:21:11 +0100

    Fix mobile issue and add includes

--- app/packs/src/components/notifications/index.jsx
+++ app/packs/src/components/notifications/index.jsx
@@ -94,7 +94,7 @@ const Notifications = ({ notifications }) => {
 
   return (
     <>
-      <Dropdown align="left">
+      <Dropdown drop="bottom">
         <Dropdown.Toggle
           className="user-menu-dropdown-btn no-caret"
           id="notifications-dropdown"
