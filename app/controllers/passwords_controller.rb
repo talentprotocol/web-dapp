d784a306135ae8724345b701d18d21f7f1dd9fd7
francisco-leal <francisco@talentprotocol.com>
2021-10-21 12:37:55 +0100

    [Mailer] Forgot password use mandril for the email

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,11 @@
+class PasswordsController < Clearance::PasswordsController
+  def create
+    if user = find_user_for_create
+      user.forgot_password!
+
+      UserMailer.with(user: user).send_password_reset_email.deliver_later
+    end
+
+    render template: "passwords/create"
+  end
+end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,11 @@
 class UserMailer < ApplicationMailer
   def send_sign_up_email
     @user = params[:user]
-    mail(to: @user.email, subject: 'Welcome to Talent Protocol')
+    mail(to: @user.email, subject: "Welcome to Talent Protocol")
+  end
+
+  def send_password_reset_email
+    @user = params[:user]
+    mail(to: @user.email, subject: "Talent Protocol - Password reset")
   end
 end
