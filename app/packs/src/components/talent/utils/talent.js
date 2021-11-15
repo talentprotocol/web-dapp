4b40d892d5499cef1e3c4960d261f09d9e7d5a04
francisco-leal <francisco@talentprotocol.com>
2021-11-15 10:05:49 +0000

    [Profile] Tweak limitations and required fields

--- app/packs/src/components/talent/utils/talent.js
+++ app/packs/src/components/talent/utils/talent.js
@@ -1,39 +1,22 @@
 export const completeProfile = ({
   talent,
-  primary_tag,
   profilePictureUrl,
   token,
   career_goal,
-  milestones,
-  goals,
 }) => {
   const validOccupation =
     talent.profile?.occupation && talent.profile.occupation != "";
-  const validLocation =
-    talent.profile?.location && talent.profile.location != "";
   const validHeadline =
     talent.profile?.headline && talent.profile.headline != "";
-  const validPrimaryTag = primary_tag && primary_tag != "";
   const validProfilePicture = profilePictureUrl && profilePictureUrl != "";
   const validTokenTicker = token.ticker && token.ticker != "";
-  const validCareerGoalBio = career_goal?.bio && career_goal.bio != "";
   const validCareerGoalPitch = career_goal?.pitch && career_goal.pitch != "";
-  const validCareerGoalChallenges =
-    career_goal?.challenges && career_goal.challenges != "";
-  const validMilestones = milestones.length > 0;
-  const validGoals = goals.length > 0;
 
   return (
     !!validOccupation &&
-    !!validLocation &&
-    !!validHeadline &&
-    !!validPrimaryTag &&
     !!validProfilePicture &&
     !!validTokenTicker &&
-    !!validCareerGoalBio &&
-    !!validCareerGoalPitch &&
-    !!validCareerGoalChallenges &&
-    !!validMilestones &&
-    !!validGoals
+    !!validHeadline &&
+    !!validCareerGoalPitch
   );
 };
