145781de94efc2c1d5f0c526578b4c8123fb1244
Andreas Vilela <andreasvilela@gmail.com>
2021-11-25 11:38:52 +0000

    #card wip

---
+++ app/packs/src/components/design_system/cards/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Card = ({
+    mode,
+    photo_url,
+    name,
+    title,
+    circ_supply,
+    ticker,
+}) => {
+
+    return (
+    <div className={`col-md-3 card ${mode}`}>
+        <img class="card-img-top" src={`${photo_url}`} alt="Profile picture"></img>
+
+        <div class="card-body">
+                <h6>{name}</h6>
+                <span className={`title ${mode}`}>{title}</span>
+                <p className={`circ-supply ${mode}`}>CIRCULATING SUPPLY</p>
+                <strong className={`circ-supply-data ${mode}`}>{circ_supply} <span className={`ticker ${mode}`}>{ticker}</span></strong>
+        </div>
+    </div>
+    )
+};
+
+export default Card;
