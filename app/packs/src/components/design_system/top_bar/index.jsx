60738d456044707f8fd3f973704d9931b1af2c5b
Andreas Vilela <andreasvilela@gmail.com>
2021-12-13 16:41:41 +0000

    #top bar dark mode, improvements and state of connected wallet

--- app/packs/src/components/design_system/top_bar/index.jsx
+++ app/packs/src/components/design_system/top_bar/index.jsx
@@ -1,31 +1,61 @@
 import React from 'react';
-import ButtonProfile from 'src/components/design_system/other/button-profile';
-import ButtonIconNotifications from 'src/components/design_system/other/button-icon-notifications';
-import ButtonConnectWallet from 'src/components/design_system/other/button-connect-wallet';
-import ButtonIconArrowBack from 'src/components/design_system/other/button-icon-arrowback';
-import ButtonIconArrowForward from 'src/components/design_system/other/button-icon-arrowforward';
+import ArrowBack from 'src/components/icons/ArrowBack';
+import ArrowForward from 'src/components/icons/ArrowForward';
+import Bell from 'src/components/icons/Bell';
+import Metamask from 'src/components/icons/Metamask';
+import ArrowDown from 'src/components/icons/ArrowDown';
+
 
 const TopBar = ({
-    type,
     mode,
-    title,
-    message,
-    link,
-    link_text
+    name,
+    photo_url,
+    count_notifications = 0,
+    is_connected = false,
+    wallet_balance,
+    wallet_address
 }) => {
 
     return (
         <>
-            <nav className="navbar">
+            <nav className={`navbar ${mode}`}>
                 <div className="col-lg-6">
-                    <ButtonIconArrowBack />
-                    <ButtonIconArrowForward />
+                    <button className={`button-topbar button-square ${mode}`}>
+                        <ArrowBack color={`${mode === 'dark' ? "#FFFFFF" : "#202122"}`} />
+                    </button>
+
+                    <button className={`button-topbar button-square ml-1 ${mode}`} disabled>
+                        <ArrowForward color={`${mode === 'dark' ? "#FFFFFF" : "#202122"}`}/>
+                    </button>
                 </div>
                 <div className="col-lg-6 text-right">
-                    <ButtonConnectWallet />
-                    <ButtonProfile name="Andreas V" photo_url="https://media.istockphoto.com/photos/beautiful-woman-posing-against-dark-background-picture-id638756792?k=20&m=638756792&s=612x612&w=0&h=PAiwpR6vmkBlctx0kmvGKX3HsBcMdd2PFD4BlEEI7Ac=" />
-                    <ButtonIconNotifications />
 
+                    {is_connected == false ? 
+                        <button className={`button-topbar mr-1 ${mode}`}>
+                            <Metamask />
+                            <strong className="m-2">Connect Wallet</strong>
+                        </button>
+
+                        :
+
+                        <button className={`button-topbar mr-1 ${mode}`}>
+                            {wallet_balance ? <strong className={`m2 wallet_balance ${mode}`}>{wallet_balance}</strong> : null} 
+                            {wallet_address ? <strong className={`wallet_address mr-1 ${mode}`}> {wallet_address}</strong> : null}
+                        </button>}
+
+                    
+
+                    <button className={`button-topbar mr-1 ${mode}`}>
+                        <img className="column table-img" src={`${photo_url}`} alt="Profile picture"></img>
+
+                        <strong className="ml-1 mr-1">{name}</strong>
+                        <ArrowDown color={`${mode === 'dark' ? "#FFFFFF" : "#202122"}`} />
+                    </button>
+
+                    <button className={`button-topbar button-square ${mode}`}>
+                        <Bell color={`${mode === 'dark' ? "#FFFFFF" : "#202122"}`} />
+                        {count_notifications > 0 ? <div className="mr-1 ellipse-notifications"></div> : null}
+                    </button>
 
                 </div>
             </nav>
--- app/packs/src/components/icons/ArrowForward.jsx
+++ app/packs/src/components/icons/ArrowForward.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Icon from "src/components/design_system/icon";
 
-const Icon = (props) => (
-    <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0.666672 0.5L7.81334 7.64667C7.85978 7.69304 7.89663 7.74812 7.92176 7.80875C7.9469 7.86938 7.95984 7.93437 7.95984 8C7.95984 8.06563 7.9469 8.13062 7.92176 8.19125C7.89663 8.25188 7.85978 8.30696 7.81334 8.35333L0.666672 15.5" stroke="Black" stroke-linecap="round" stroke-linejoin="round" />
-    </svg>
-
+const ArrowForward = (props) => (
+    <Icon
+        path="M0.666672 0.5L7.81334 7.64667C7.85978 7.69304 7.89663 7.74812 7.92176 7.80875C7.9469 7.86938 7.95984 7.93437 7.95984 8C7.95984 8.06563 7.9469 8.13062 7.92176 8.19125C7.89663 8.25188 7.85978 8.30696 7.81334 8.35333L0.666672 15.5"
+        {...props}
+    />
 );
 
-export default Icon;
+export default ArrowForward;
