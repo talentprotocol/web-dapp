741ca0cab0c9907957dccd7ef58abb591871912c
francisco-leal <francisco@talentprotocol.com>
2021-12-16 19:50:19 +0000

    [UI] Tweak Sidebar look

--- app/packs/src/components/design_system/side_bar/index.jsx
+++ app/packs/src/components/design_system/side_bar/index.jsx
@@ -1,56 +1,42 @@
 import React from "react";
 import LogoLight from "src/components/icons/Logo-Light";
 import LogoDark from "src/components/icons/Logo-Dark";
-import Home from "src/components/icons/Home";
 import Talent from "src/components/icons/Talent";
 import Wallet from "src/components/icons/Wallet";
 import Chat from "src/components/icons/Chat";
 
-const SideBar = ({ mode }) => {
+import SideBarItem from "./SideBarItem";
+
+const SideBar = ({ mode, talentPath, portfolioPath, messagesPath }) => {
   return (
-    <>
-      <div className={`side-nav ${mode}`}>
-        {mode === "light" ? <LogoLight /> : <LogoDark />}
-
-        <ul className={`menu ${mode}`}>
-          <li>
-            <a href="#" className={`d-flex ${mode} active`}>
-              <div className="menu-icon">
-                <Home color={`${mode === "dark" ? "#AAADB3" : "#686C74"}`} />
-              </div>
-              <span>Home</span>
-            </a>
-          </li>
-
-          <li>
-            <a href="#" className={`d-flex ${mode}`}>
-              <div className="menu-icon">
-                <Talent color={`${mode === "dark" ? "#AAADB3" : "#686C74"}`} />
-              </div>
-              <span>Talent</span>
-            </a>
-          </li>
-
-          <li>
-            <a href="#" className={`d-flex ${mode}`}>
-              <div className="menu-icon">
-                <Wallet color={`${mode === "dark" ? "#AAADB3" : "#686C74"}`} />
-              </div>
-              <span>Portfolio</span>
-            </a>
-          </li>
-
-          <li>
-            <a href="#" className={`d-flex ${mode}`}>
-              <div className="menu-icon">
-                <Chat color={`${mode === "dark" ? "#AAADB3" : "#686C74"}`} />
-              </div>
-              <span>Messages</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </>
+    <div className={`side-nav position-fixed ${mode}`}>
+      {mode === "light" ? (
+        <LogoLight className="mt-3 ml-2" />
+      ) : (
+        <LogoDark className="mt-3 ml-2" />
+      )}
+      <ul className={`menu ${mode} d-flex flex-column`}>
+        <SideBarItem
+          mode={mode}
+          url={talentPath}
+          secondaryUrl={"/"}
+          routeName="Talent"
+          Icon={Talent}
+        />
+        <SideBarItem
+          mode={mode}
+          url={portfolioPath}
+          routeName="Portfolio"
+          Icon={Wallet}
+        />
+        <SideBarItem
+          mode={mode}
+          url={messagesPath}
+          routeName="Messages"
+          Icon={Chat}
+        />
+      </ul>
+    </div>
   );
 };
 
--- app/packs/src/components/icons/Chat.jsx
+++ app/packs/src/components/icons/Chat.jsx
@@ -5,6 +5,7 @@ const Chat = (props) => (
   <Icon
     path="M8.5.5a6.99 6.99 0 00-5.945 10.683L.5 15.5l4.316-2.055A7 7 0 108.5.5v0z"
     {...props}
+    color="currentColor"
   />
 );
 
