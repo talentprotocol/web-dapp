56e33cf3c26a26b359b69ab36bb10ec5ee37c6f9
francisco-leal <francisco@talentprotocol.com>
2021-12-28 16:34:28 +0000

    [Security] Add captcha to registration form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,24 +16,40 @@ class UsersController < ApplicationController
   end
 
   def create
-    service = CreateUser.new
-    @result = service.call(
-      email: user_params[:email],
-      username: user_params[:username],
-      password: user_params[:password],
-      invite_code: user_params[:code]
-    )
+    if verify_captcha
+      service = CreateUser.new
+      @result = service.call(
+        email: user_params[:email],
+        username: user_params[:username],
+        password: user_params[:password],
+        invite_code: user_params[:code]
+      )
 
-    if @result[:success]
-      render json: @result[:user], status: :created
+      if @result[:success]
+        render json: @result[:user], status: :created
+      else
+        render json: @result, status: :conflict
+      end
     else
-      render json: @result, status: :conflict
+      render json: {error: "We were unable to validate your captcha.", field: "captcha"}, status: :conflict
     end
   end
 
   private
 
   def user_params
-    params.permit(:email, :username, :password, :code)
+    params.permit(:email, :username, :password, :code, :captcha)
+  end
+
+  def verify_captcha
+    request = Faraday.post("https://www.google.com/recaptcha/api/siteverify",
+      {
+        secret: ENV["RECAPTCHA_SECRET_KEY"],
+        response: user_params[:captcha]
+      },
+      {"Content-Type": "application/x-www-form-urlencoded"})
+
+    result = JSON.parse(request.body)
+    result["success"]
   end
 end
