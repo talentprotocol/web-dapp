3f1cdea96064fa24f3016158c3ed0711b7c83fe9
francisco-leal <francisco@talentprotocol.com>
2022-01-25 10:55:34 +0000

    Add formatted dates to talent show

--- app/packs/src/components/design_system/cards/roadmap.jsx
+++ app/packs/src/components/design_system/cards/roadmap.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import dayjs from "dayjs";
+
 import Caption from "src/components/design_system/typography/caption";
 
 const Roadmap = ({ mode, due_date, title, description, className = "" }) => {
+  const formatedDueDate = dayjs(due_date, "YYYY-MM-DD").format("YYYY-MM");
+
   return (
     <div className={`card ${mode} ${className}`}>
       <Caption
         className="roadmap-date text-uppercase text-primary"
         mode={`${mode}`}
-        text={`${due_date}`}
+        text={`${formatedDueDate}`}
       />
       <Caption
         className="roadmap-title"
