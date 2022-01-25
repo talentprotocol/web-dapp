84555414407f5de0e9a91f75ab40b61546efe4e1
francisco-leal <francisco@talentprotocol.com>
2022-01-25 16:57:53 +0000

    Add card hover and change background color of primary-default disabled

--- app/packs/src/components/design_system/cards/perk.jsx
+++ app/packs/src/components/design_system/cards/perk.jsx
@@ -24,7 +24,7 @@ const Perk = ({
 
   return (
     <div
-      className={`card${
+      className={`card card-hover${
         hideAction ? "" : " cursor-pointer"
       } ${mode} ${className}`}
       onClick={goToRoute}
