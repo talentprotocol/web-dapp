76211a34fe33b1957f2e61032a4789ea83b436bf
Ruben Sousa Dinis <ruben.sousa.dinis@gmail.com>
2022-01-29 10:53:18 +0000

    Add service to send message per receiver

--- app/jobs/send_message_to_all_supporters_job.rb
+++ app/jobs/send_message_to_all_supporters_job.rb
@@ -1,12 +1,27 @@
 class SendMessageToAllSupportersJob < ApplicationJob
   queue_as :default
 
-  def perform(user_id, message)
-    user = User.find(user_id)
+  def perform(sender_id, message)
+    sender = User.find(sender_id)
 
-    Messages::SendToAllSupporters.new(
-      message: "Thanks for the support!",
-      user: user
-    ).call
+    investors(sender).find_each do |investor|
+      send_message_service.call(
+        message: message,
+        sender: sender,
+        receiver: investor.user
+      )
+    end
+  end
+
+  private
+
+  def investors(sender)
+    return [] unless sender.talent
+
+    sender.talent.investors.where.not(user: sender).distinct.includes(:user)
+  end
+
+  def send_message_service
+    @send_message_service ||= Messages::Send.new
   end
 end
---
+++ spec/services/messages/send_spec.rb
@@ -0,0 +1,57 @@
+require "rails_helper"
+
+RSpec.describe Messages::Send do
+  subject(:send_message) { described_class.new.call(sender: sender, receiver: receiver, message: message) }
+
+  let(:message) { "Thanks for you support!" }
+  let(:sender) { create :user }
+  let(:receiver) { create :user }
+
+  let(:create_notification_class) { CreateNotification }
+  let(:create_notification_instance) { instance_double(create_notification_class, call: true) }
+
+  let(:action_cable_server) do
+    instance_double(ActionCable::Server::Base, broadcast: true)
+  end
+
+  before do
+    allow(create_notification_class).to receive(:new).and_return(create_notification_instance)
+    allow(ActionCable).to receive(:server).and_return(action_cable_server)
+  end
+
+  it "creates two messages" do
+    expect { send_message }.to change(Message, :count).from(0).to(1)
+  end
+
+  it "returns the message sent" do
+    created_message = send_message
+
+    expect(created_message).to eq sender.messaged.first
+  end
+
+  it "initializes and calls the create notification service" do
+    send_message
+
+    expect(create_notification_class).to have_received(:new)
+    expect(create_notification_instance).to have_received(:call).with(
+      title: "New message",
+      body: "You have a new message",
+      user_id: receiver.id,
+      type: "Notifications::MessageReceived"
+    )
+  end
+
+  it "broadcasts the created message" do
+    send_message
+
+    expect(action_cable_server).to have_received(:broadcast)
+  end
+
+  context 'when the sender and receiver are the same' do
+    let(:receiver) { sender }
+
+    it "does not send a new message" do
+      expect { send_message }.not_to change(Message, :count)
+    end
+  end
+end
