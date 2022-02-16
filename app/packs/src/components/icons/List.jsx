0083e1d01b4e55d731c4c265aa5a99e3451a4be9
Frederico Moura <verdefred@hotmail.com>
2022-02-16 16:39:47 +0000

    New icons

---
+++ app/packs/src/components/icons/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Icon from "src/components/design_system/icon";
+
+const List = (props) => (
+  <Icon
+    path="M0 .498h16v3.667H0V.498zM0 6.164h16v3.667H0V6.164zM0 11.832h16v3.667H0v-3.667z"
+    {...props}
+  />
+);
+
+export default List;
