a813f87eec1d8b4b841ebd6b26ae2d7831703aa0
francisco-leal <francisco@talentprotocol.com>
2022-03-02 16:41:26 +0000

    Refactor digest query

--- app/jobs/send_notification_digests_job.rb
+++ app/jobs/send_notification_digests_job.rb
@@ -2,13 +2,17 @@ class SendNotificationDigestsJob < ApplicationJob
   queue_as :default
 
   def perform
-    notifications = Notification.where(read_at: nil, emailed_at: nil)
-      .where("created_at >= ?", 2.day.ago)
-      .pluck(:recipient_id, :id).group_by(&:first)
-      .transform_values { |pairs| pairs.map(&:second) }
+    notifications = Notification
+      .where(read_at: nil, emailed_at: nil)
+      .where("created_at >= ?", 2.days.ago)
+      .group(:recipient_id)
+      .select(
+        "recipient_id",
+        'array_agg("id") as notification_ids'
+      )
 
-    notifications.each do |user_id, notification_ids|
-      NotificationMailer.digest(user_id, notification_ids).deliver_later
+    notifications.each do |row|
+      NotificationMailer.digest(row.recipient_id, row.notification_ids).deliver_later
     end
   end
 end
