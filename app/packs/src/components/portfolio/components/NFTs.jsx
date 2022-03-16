7d56fa9fbfb931fb2a7cb3c516f06a9c0b5a2904
francisco-leal <francisco@talentprotocol.com>
2022-03-16 11:04:31 +0000

    Add NFT image loading

--- app/packs/src/components/portfolio/components/NFTs.jsx
+++ app/packs/src/components/portfolio/components/NFTs.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { LoadingPortfolio } from "../NewPortfolio";
 
-const NFTs = () => {
+const NFTs = ({ userNFT, chainAPI }) => {
   const [loading, setLoading] = useState(true);
+  const [nftImages, setNFTImages] = useState({});
 
+  useEffect(() => {
+    chainAPI.getNFTImg(userNFT.tokenAddress, userNFT.id).then((imageURL) => {
+      setNFTImages((prev) => ({ ...prev, userNFT: imageURL }));
+      setLoading(false);
+    });
+  }, [userNFT]);
+
+  console.log(nftImages);
   if (loading) {
     return <LoadingPortfolio />;
   }
-  return <h1>NFTs!</h1>;
+  return (
+    <>
+      <h1>NFTs!</h1>
+      <img src={nftImages.userNFT} width="100" height="100" />
+    </>
+  );
 };
 
 export default NFTs;
