85082266e3f31401ee4501901390c37547b85af8
Frederico Moura <verdefred@hotmail.com>
2022-03-22 00:08:26 +0000

    Fix portfolio issues

--- app/packs/src/components/portfolio/NewPortfolio.jsx
+++ app/packs/src/components/portfolio/NewPortfolio.jsx
@@ -158,7 +158,7 @@ const NewPortfolio = ({
       skip: page * PAGE_SIZE,
       first: PAGE_SIZE,
     },
-    skip: !localAccount || wrongChain,
+    skip: !localAccount,
   });
 
   // --- Interface variables ---
@@ -393,6 +393,16 @@ const NewPortfolio = ({
 
   if (wrongChain) {
     return <ChangeNetwork mode={theme.mode()} networkChange={networkChange} />;
+  }
+
+  if (!walletConnected) {
+    return (
+      <ConnectWallet
+        userId={currentUserId}
+        onConnect={onWalletConnect}
+        railsContext={railsContext}
+      />
+    );
   }
 
   if (mobile) {
