e8c54263e662852ae77ce68920933fbdfa83b021
Frederico Moura <verdefred@hotmail.com>
2022-03-22 11:53:52 +0000

    Restore correct thegraph constants (#415)

--- app/packs/src/utils/constants.js
+++ app/packs/src/utils/constants.js
@@ -7,9 +7,8 @@ export const THE_GRAPH_ENDPOINTS = {
   production:
     "https://api.studio.thegraph.com/query/10292/talent-tokens/v0.0.5",
   staging:
-    "https://api.studio.thegraph.com/query/10292/talent-protocol/v0.0.18",
-  development:
-    "https://api.thegraph.com/subgraphs/name/verdefred/celo-alfajores-dev",
+    "https://api.studio.thegraph.com/query/10292/talent-protocol/v0.0.19",
+  development: "https://api.studio.thegraph.com/query/8098/mvp/v0.0.23",
 };
 
 export const SUPPORTER_GUIDE =
