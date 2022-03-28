931ceb643544d296b5c61d0c2ba4e0bbabb9eb82
Frederico Moura <verdefred@hotmail.com>
2022-03-28 13:23:26 +0100

    Update thegraph prod link

--- app/packs/src/utils/constants.js
+++ app/packs/src/utils/constants.js
@@ -4,9 +4,8 @@ export const PRIVACY_HREF =
   "https://drive.google.com/file/d/1Cts7FcmstLa_IISHFeO6vizy8Um5rKZE/view";
 
 export const THE_GRAPH_ENDPOINTS = {
-  production:
-    "https://api.studio.thegraph.com/query/10292/talent-tokens/v0.0.5",
   development: "https://api.studio.thegraph.com/query/8098/mvp/v0.0.23",
+  production: "https://api.thegraph.com/subgraphs/name/talent-protocol/prod",
   staging: "https://api.thegraph.com/subgraphs/name/talent-protocol/dev",
 };
 
