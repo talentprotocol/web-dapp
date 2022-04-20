f73222d04db2d611682e3dee7707c6ac50f63ad2
Frederico Moura <verdefred@hotmail.com>
2022-04-20 12:13:02 +0100

    Search notification by source and model id before creating

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,6 +5,10 @@ class Notification < ApplicationRecord
   scope :find_by_source_id, ->(source_id) {
     where("(params->>'source_id')::bigint = ?", source_id.to_int)
   }
+  scope :find_by_source_and_model_id, ->(source_id, model_id) {
+    where("(params->>'source_id')::bigint = ?", source_id.to_int)
+      .where("(params->>'model_id')::bigint = ?", model_id.to_int)
+  }
   scope :unemailed, -> { where(emailed_at: nil) }
 
   def mark_as_emailed
--- app/services/create_notification.rb
+++ app/services/create_notification.rb
@@ -1,8 +1,11 @@
 class CreateNotification
   def call(recipient:, type:, source_id: nil, model_id: nil)
-    notification = get_existing_unread(recipient: recipient,
-                                       type: type,
-                                       source_id: source_id)
+    notification = get_existing_unread(
+      recipient: recipient,
+      type: type,
+      source_id: source_id,
+      model_id: model_id
+    )
     if notification.present?
       notification.updated_at = Time.current
       notification.save!
@@ -18,10 +21,12 @@ class CreateNotification
 
   private
 
-  def get_existing_unread(recipient:, type:, source_id:)
-    notifications = Notification.where(type: type.name, recipient: recipient,
-                                       read_at: nil)
-    if source_id
+  def get_existing_unread(recipient:, type:, source_id:, model_id:)
+    notifications = Notification.where(type: type.name, recipient: recipient, read_at: nil)
+
+    if source_id && model_id
+      notifications = notifications.find_by_source_and_model_id(source_id, model_id)
+    elsif source_id
       notifications = notifications.find_by_source_id(source_id)
     end
     notifications.last
