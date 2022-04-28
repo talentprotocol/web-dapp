9703427b64e2b242388765a1955c42ee326ee82e
taru5 <guptapallavi955@gmail.com>
2022-04-28 11:13:57 +0530

    Use spaces for indentation instead of tabs

--- app/services/talents/send_token_notification_to_discord.rb
+++ app/services/talents/send_token_notification_to_discord.rb
@@ -1,23 +1,23 @@
 module Talents
     class SendTokenNotificationToDiscord
-   
+        
         include Rails.application.routes.url_helpers
-
+        
         def initialize
             @url = ENV["DISCORD_WEBHOOK_URL"]
         end
-
+        
         def call(token_id)
             token = Token.find(token_id)
             talent = token.talent
             user = talent.user
-
+            
             message = "_#{user.username}_ is _#{talent.profile[:occupation]}_ and just launched their Talent Token **$#{token.ticker}**. You can take a look at their profile here: #{user_url(talent)}"
             post(message)
         end
-
+        
         private
-
+        
         def post(message)
             Faraday.post(
                 @url.to_s,
