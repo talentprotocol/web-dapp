069fc36d29067e915b3b113836dd711896504c66
Frederico Moura <verdefred@hotmail.com>
2022-04-28 11:50:43 +0100

    New user menu

---
+++ app/packs/src/components/icons/Edit.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Icon from "src/components/design_system/icon";
+
+const Edit = (props) => (
+  <Icon
+    path="M14.793 1.207a2.426 2.426 0 00-3.446.023L1.68 10.897.5 15.5l4.603-1.18 9.667-9.667a2.426 2.426 0 00.023-3.446v0zM11.07 1.507l3.423 3.422M1.68 10.896l3.427 3.42"
+    color="currentColor"
+    {...props}
+  />
+);
+
+export default Edit;
---
+++ app/packs/src/components/icons/Settings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Icon from "src/components/design_system/icon";
+
+const Settings = (props) => (
+  <Icon
+    path="M.499 8.167a4 4 0 004 4h7a4 4 0 000-8h-7a4 4 0 00-4 4v0zM11.499 6.167v4M9.499 6.167v4"
+    color="currentColor"
+    {...props}
+  />
+);
+
+export default Settings;
