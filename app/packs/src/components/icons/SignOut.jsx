4a7d6bba5fabc0cbe8ad262b5c4f13e21342abe3
Frederico Moura <verdefred@hotmail.com>
2022-04-28 11:52:04 +0100

    New user menu (#473)

---
+++ app/packs/src/components/icons/SignOut.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Icon from "src/components/design_system/icon";
+
+const SignOut = (props) => (
+  <Icon
+    path="M.5 8.003H11M8.5 10.503l2.5-2.5-2.5-2.5M2.204 11.067a7 7 0 10.12-6.362"
+    color="currentColor"
+    {...props}
+  />
+);
+
+export default SignOut;
---
+++ app/packs/src/components/icons/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Icon from "src/components/design_system/icon";
+
+const User = (props) => (
+  <Icon
+    path="M4.5 4a3.5 3.5 0 107 0 3.5 3.5 0 00-7 0v0zM1.5 15.5a6.5 6.5 0 0113 0"
+    color="currentColor"
+    {...props}
+  />
+);
+
+export default User;
