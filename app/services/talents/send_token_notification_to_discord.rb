d3b6177c162e0b1e5bc715b324e92c9c9aa76c66
taru5 <guptapallavi955@gmail.com>
2022-04-29 22:39:34 +0530

    Fix indentation to 2 spaces

--- app/services/talents/send_token_notification_to_discord.rb
+++ app/services/talents/send_token_notification_to_discord.rb
@@ -1,33 +1,33 @@
 module Talents
-    class SendTokenNotificationToDiscord
-        
-        include Rails.application.routes.url_helpers
-        
-        def initialize
-            @url = ENV["DISCORD_WEBHOOK_URL"]
-        end
-        
-        def call(token_id)
-            token = Token.find(token_id)
-            talent = token.talent
-            user = talent.user
-            
-            message = "_#{user.username}_ is _#{talent.profile[:occupation]}_ and just launched their Talent Token **$#{token.ticker}**. You can take a look at their profile here: #{user_url(talent)}"
-            post(message)
-        end
-        
-        private
-        
-        def post(message)
-            Faraday.post(
-                @url.to_s,
-                {
-                    content: message,
-                }.to_json,
-                {
-                    "Content-Type": "application/json",
-                }
-            )
-        end
+  class SendTokenNotificationToDiscord
+    
+    include Rails.application.routes.url_helpers
+    
+    def initialize
+      @url = ENV["DISCORD_WEBHOOK_URL"]
     end
+    
+    def call(token_id)
+      token = Token.find(token_id)
+      talent = token.talent
+      user = talent.user
+      
+      message = "_#{user.username}_ is _#{talent.profile[:occupation]}_ and just launched their Talent Token **$#{token.ticker}**. You can take a look at their profile here: #{user_url(talent)}"
+      post(message)
+    end
+    
+    private
+    
+    def post(message)
+      Faraday.post(
+        @url.to_s,
+        {
+          content: message,
+        }.to_json,
+        {
+          "Content-Type": "application/json",
+        }
+      )
+    end
+  end
 end
