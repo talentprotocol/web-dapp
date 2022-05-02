ccb9f338eb1d20931d155f3a1bf16a84c4070d55
Pallavi Gupta <31515330+taru5@users.noreply.github.com>
2022-05-02 21:29:29 +0530

    Add discord webhook message on talent token launch (#466)
    * Add discord webhook message on talent token launch
    
    * Move logic for discord notification from job to service
    
    * Add new empty line to env file
    
    * Delete minitest for Discord notification job
    
    * Use spaces for indentation instead of tabs
    
    * Fix indentation to 2 spaces

---
+++ app/jobs/send_token_notification_to_discord_job.rb
@@ -0,0 +1,9 @@
+class SendTokenNotificationToDiscordJob < ApplicationJob
+  queue_as :default
+
+  def perform(token_id)
+    service = Talents::SendTokenNotificationToDiscord.new
+    service.call(token_id)
+  end
+
+end
