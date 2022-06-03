6ffa577c4a8ca6d38a989f6370c4dc20773c2878
Frederico Moura <verdefred@hotmail.com>
2022-06-03 15:27:10 +0100

    Talent invite creates a talent profile type

--- app/services/create_user.rb
+++ app/services/create_user.rb
@@ -19,6 +19,7 @@ class CreateUser
       if invite&.talent_invite?
         create_talent(user)
         create_token(user)
+        update_profile_type(user)
       end
 
       create_invite(user)
@@ -112,5 +113,9 @@ class CreateUser
     if invite.user.invites.where(talent_invite: true).sum(:uses) > 4
       UpdateTasksJob.perform_later(type: "Tasks::Register", user_id: invite.user.id)
     end
+  end
+
+  def update_profile_type(user)
+    user.talent!
   end
 end
