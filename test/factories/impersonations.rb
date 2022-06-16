d06ba1feb042ebac64392000342fa45b0ac5524d
taru5 <guptapallavi955@gmail.com>
2022-06-16 22:36:32 +0530

    Remove unnecessary files

--- test/factories/impersonations.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :impersonation do
-    impersonator { nil }
-    impersonated { nil }
-    ip { "MyString" }
-  end
-end
--- test/models/impersonation_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ImpersonationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
