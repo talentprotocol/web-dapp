43a977327daa56292fef539b278c7275debc4a48
taru5 <guptapallavi955@gmail.com>
2022-07-06 22:47:26 +0530

    Added mail previews

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,4 +3,24 @@ class UserMailerPreview < ActionMailer::Preview
   def send_sign_up_email
     UserMailer.with(user_id: User.first.id).send_sign_up_email
   end
+
+  def send_welcome_email_talent
+    UserMailer.with(user: Talent.first.user).send_welcome_email
+  end
+
+  def send_welcome_email_investor
+    UserMailer.with(user: Investor.first.user).send_welcome_email
+  end
+
+  def send_token_launch_reminder_email
+    UserMailer.with(user: User.first).send_token_launch_reminder_email
+  end
+
+  def send_token_purchase_reminder_email
+    UserMailer.with(user: User.first).send_token_purchase_reminder_email
+  end
+
+  def send_talent_upgrade_email
+    UserMailer.with(user: User.first).send_talent_upgrade_email
+  end
 end
