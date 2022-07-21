24f0bfc6d08c9c841cf64c2a6f9e8ba6408db212
Frederico Moura <verdefred@hotmail.com>
2022-07-21 11:35:32 +0100

    Fix update profile type (#627)
    * Fix update profile type
    
    * Update safe guard
    
    * Add rake task to delete incorrect records

--- app/services/users/update_profile_type.rb
+++ app/services/users/update_profile_type.rb
@@ -1,15 +1,16 @@
 module Users
   class UpdateProfileType
-    def call(user_id:, new_profile_type:, who_dunnit_id: nil)
-      user = User.find(user_id)
+    def call(user:, new_profile_type:, who_dunnit_id: nil)
       previous_profile_type = user.profile_type
+
+      return if previous_profile_type == new_profile_type || previous_profile_type === "talent"
 
       user.update!(profile_type: new_profile_type)
       UserMailer.with(user: user).send_talent_upgrade_email.deliver_later(wait: 5.seconds) if new_profile_type == "approved"
 
       UserProfileTypeChange.create!(
-        user_id: user_id,
-        who_dunnit_id: who_dunnit_id || user_id,
+        user_id: user.id,
+        who_dunnit_id: who_dunnit_id || user.id,
         previous_profile_type: previous_profile_type,
         new_profile_type: new_profile_type
       )
